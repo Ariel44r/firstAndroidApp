e3cd52ac55418a0ce596e40519833c2c8d2e5adb
ariel <ariel44r@gmail.com>
2018-06-29 01:40:33 -0500

    add action to button

--- app/src/main/java/com/example/ariel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ariel/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ariel.myapplication;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
         user = (EditText)findViewById(R.id.txtUser);
         password = (EditText)findViewById(R.id.txtPassword);
         login = (Button)findViewById(R.id.btnLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = user.getText().toString();
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Hey!");
+                alertDialog.setMessage("Hello " + userName + ", how are you today dude?");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "I'm OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+
+                        }
+                    }
+                );
+                alertDialog.show();
+
+            }
+        });
     }
 }
