8c8bea6c625582a39f1f1b7c854b572927412ab8
ariel <ariel44r@gmail.com>
2018-06-29 00:28:29 -0500

    [ first compile on MyHuawei ]

--- app/src/main/java/com/example/ariel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ariel/myapplication/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.ariel.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText user;
+    private EditText password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (EditText)findViewById(R.id.txtUser);
+        password = (EditText)findViewById(R.id.txtPassword);
+        login = (Button)findViewById(R.id.btnLogin);
     }
 }
